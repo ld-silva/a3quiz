09ab879ab3e4a7007bf7bfd770fe75412f39b34b
Lucas Francelino <61632300+ld-silva@users.noreply.github.com>
2024-11-17 00:36:31 -0300

    Update script.js

--- A3/script.js
+++ A3/script.js
@@ -1,4 +1,5 @@
 function checkAnswers() {
+    //Guarda as respostas (Gabarito)
     const answers = {
       q1: "c",
       q2: "b",
@@ -11,18 +12,21 @@ function checkAnswers() {
       q9: "b",
       q10: "a",
     };
-  
+
+    //Obtém as respostas selecionadas
     let score = 0;
     const form = document.getElementById("quiz-form");
     const result = document.getElementById("result");
-  
+
+    //Faz a contagem de acertos e erros
     for (const question in answers) {
       const userAnswer = form[question].value;
       if (userAnswer === answers[question]) {
         score++;
       }
     }
-  
+
+    //Mostra o resultado
     result.textContent = `Você acertou ${score} de ${Object.keys(answers).length} perguntas!`;
     result.style.color = score === Object.keys(answers).length ? "green" : "red";
   }
